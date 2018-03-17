6fb28503eea2d2651b43133f4cd09ceade5b819e
Alex Miles <alex@studiomile.com>
2018-03-17 17:45:41 -0400

    Remove eslint erros from model initialization

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -4,13 +4,15 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require('../config/config.js')[env];
+
 const db = {};
 
+let sequelize;
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
