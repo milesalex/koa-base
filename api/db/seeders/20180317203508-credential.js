5bf414fde61a8116defd3cf4c7c7437d811c3dca
Alex Miles <alex@studiomile.com>
2018-03-17 17:03:16 -0400

    Get migration working

--- api/db/seeders/20180317203508-credential.js
+++ api/db/seeders/20180317203508-credential.js
@@ -1,26 +1,34 @@
-'use strict';
-
 module.exports = {
-  up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+  up(queryInterface, Sequelize) {
+    // Add altering commands here.
+    // Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert(
+      'credential',
+      [
+        {
+          type: 'gdax',
+          credentials: JSON.stringify({ key: 'abc', secret: 'xyz' }),
+          readOnly: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          type: 'coinbase',
+          credentials: JSON.stringify({ key: 'abc', secret: 'xyz' }),
+          readOnly: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
   },
 
-  down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+  down(queryInterface, Sequelize) {
+    // Add reverting commands here.
+    // Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-  }
+    return queryInterface.bulkDelete('Credential', null, {});
+  },
 };
