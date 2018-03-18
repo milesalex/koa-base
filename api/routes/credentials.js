556e97295055e41853708258c1b5a06cb1e319a5
Alex Miles <alex@studiomile.com>
2018-03-18 01:37:12 -0400

    Add more tests and endpoint

--- api/routes/credentials.js
+++ api/routes/credentials.js
@@ -4,13 +4,35 @@ const models = require('../db/models');
 const router = new Router();
 const BASE_URL = '/api/credentials';
 
+// Get all
 router.get(BASE_URL, async (ctx) => {
   try {
     const credentials = await models.Credential.findAll();
-
     ctx.body = {
+      status: 'success',
       data: credentials,
     };
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Get by id
+router.get(`${BASE_URL}/:id`, async (ctx) => {
+  try {
+    const credential = await models.Credential.findById(ctx.params.id);
+    if (credential) {
+      ctx.body = {
+        status: 'success',
+        data: credential,
+      };
+    } else {
+      ctx.status = 404;
+      ctx.body = {
+        status: 'error',
+        message: "That credential doesn't exist",
+      };
+    }
   } catch (err) {
     console.log(err);
   }
--- test/routes.credentials.test.js
+++ test/routes.credentials.test.js
@@ -20,6 +20,36 @@ describe('routes: credentials', () => {
     expect(response.status).toEqual(200);
     expect(response.type).toEqual('application/json');
     expect(response.body.data.length).toEqual(2);
+    expect(response.body.status).toEqual('success');
+
+    expect(response.body.data[0]).toHaveProperty('type');
+    expect(response.body.data[0]).toHaveProperty('credentials');
+    expect(response.body.data[0]).toHaveProperty('lastRefreshRequest');
+    expect(response.body.data[0]).toHaveProperty('lastRefreshSuccess');
+    expect(response.body.data[0]).toHaveProperty('lastRefreshError');
+  });
+
+  test('should respond with a single credential', async () => {
+    const response = await request(server).get('/api/credentials/1');
+
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual('application/json');
+    expect(response.body.status).toEqual('success');
+
+    expect(response.body.data).toHaveProperty('type');
+    expect(response.body.data).toHaveProperty('credentials');
+    expect(response.body.data).toHaveProperty('lastRefreshRequest');
+    expect(response.body.data).toHaveProperty('lastRefreshSuccess');
+    expect(response.body.data).toHaveProperty('lastRefreshError');
+  });
+
+  test('should throw an error if a credential doesnt exist', async () => {
+    const response = await request(server).get('/api/credentials/9999');
+
+    expect(response.status).toEqual(404);
+    expect(response.type).toEqual('application/json');
+    expect(response.body.status).toEqual('error');
+    expect(response.body.message).toEqual("That credential doesn't exist");
   });
 });
 
